e3aee007fa024fb797938c1f554b0ee5330f8d35
ckrountree <jmail0207@gmail.com>
2017-12-11 09:24:49 -0800

    update pkg.json to get mjs working with mocha and pass initial test

---
+++ server/test/auth.test.mjs
@@ -0,0 +1,13 @@
+// import request from './request';
+import db from './db';
+// import { assert } from 'chai';
+
+describe('Auth TESTS', () => {
+
+    beforeEach(() => db.drop);
+
+    it('tests mjs', () => {
+
+    });
+  
+});
