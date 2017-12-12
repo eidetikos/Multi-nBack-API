a3420dfb95a33d5d56e0a0e42ba0050be5c6f1c7
Zac-L <Zachlichens@gmail.com>
2017-12-12 12:19:08 -0800

    explictly calling ensureAuth

--- server/lib/utils/ensure-auth.mjs
+++ server/lib/utils/ensure-auth.mjs
@@ -1,16 +1,14 @@
 import tokenService from './token-service';
 
-export default function() {
-    return (req, res, next) => {
-        console.log('in ensure-auth')
-        const token = req.get('Authorization') || req.get('authorization');
-        tokenService.verify(token)
-            .then(payload => {
-                req.user = payload;
-                next();
-            })
-            .catch(() => {
-                next({ code: 404, error: 'Not Authorized' });
-            });
-    };
+export default function (req, res, next) {
+    console.log('in ensure-auth');
+    const token = req.get('Authorization') || req.get('authorization');
+    tokenService.verify(token)
+        .then(payload => {
+            req.user = payload;
+            next();
+        })
+        .catch(() => {
+            next({ code: 404, error: 'Not Authorized' });
+        });
 }
