406a14186b6d7133b4f64ffe7c9f872c4b32c5c6
Zac-L <Zachlichens@gmail.com>
2017-12-14 22:11:12 -0800

    moved a few routes around, renamed routes to hopefully be more restfull

--- server/lib/routes/stats.mjs
+++ server/lib/routes/stats.mjs
@@ -1,26 +1,54 @@
-// import Game from '../models/game';
 import User from '../models/user';
-import userstats from './userstats';
 import ensureAuth from '../utils/ensure-auth';
 import express from 'express';
 
 const router = express.Router()
-    .use('/users', ensureAuth, userstats)
-    .get('/most-sequences-by-user', (req, res, next) => {
-        User.mostSequencesByUser()
-            .then( data => res.send(data))
+    .get('/community', (req, res, next) => {
+        const communityStats = [
+            User.mostSequencesByUser(),
+            User.highestNBackPerDifficulty(),
+            User.topScoresPerDifficulty()
+        ];
+        Promise.all(communityStats)
+            .then( ([
+                mostSequencesByUser,
+                highestNBackPerDifficulty,
+                topScoresPerDifficulty
+            ]) => {
+                res.send({
+                    mostSequencesByUser,
+                    highestNBackPerDifficulty: mapLeaderboardArrayToObject(highestNBackPerDifficulty),
+                    topScoresPerDifficulty: mapLeaderboardArrayToObject(topScoresPerDifficulty)
+                });
+            })
             .catch(next);
     })
-    .get('/highest-n-per-difficulty', (req, res, next) => {
-        User.highestNBackPerDifficulty()
-            .then( data => res.send(data))
-            .catch(next);
-    })
-    .get('/top-scores-per-difficulty', (req, res, next) => {
-        User.topScoresPerDifficulty()
-            .then( data => res.send(data))
+    .get('/personal', ensureAuth, (req, res, next) => {
+        const userstats = [
+            User.usersSequencesCompleted(req.user.id),
+            User.usersMaxN(req.user.id),
+            User.usersTopScore(req.user.id)
+        ];
+        Promise.all(userstats)
+            .then( ([
+                [ { sequencesCompleted } ],
+                [ { maxN } ],
+                [ { topScore } ]
+            ]) => res.send({
+                sequencesCompleted,
+                maxN,
+                topScore
+            }))
             .catch(next);
     })
 ;
 
 export default router;
+
+
+function mapLeaderboardArrayToObject(leaderboards) {
+    const tempLeaderboards = [ ...leaderboards ];
+    leaderboards = {};
+    tempLeaderboards.forEach(ele => leaderboards[ele.difficulty] = ele.leaderboard);
+    return leaderboards;
+}
