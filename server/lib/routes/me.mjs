1183562ed953fe99878e40dfeb9816f3776ef224
Zac-L <Zachlichens@gmail.com>
2017-12-14 22:10:19 -0800

    built out a me route

---
+++ server/lib/routes/me.mjs
@@ -0,0 +1,19 @@
+import User from '../models/user';
+import ensureAuth from '../utils/ensure-auth';
+import express from 'express';
+const router = express.Router();
+
+
+router
+    .get('/', ensureAuth, (req, res, next) => {
+        User.findById(req.user.id)
+            .select('-hash')
+            .lean()
+            .then(user => {
+                res.send(user);
+            })
+            .catch(next);
+    });
+
+
+export default router;
